c3d7a76d0ad6bc5e65438267c53f513347d3a712
schweppie <joeppetersmail@gmail.com>
2021-06-26 10:25:33 +0200

    Add KeyInput

---
+++ Input/KeyInput.js
@@ -0,0 +1,40 @@
+class KeyInput
+{
+    constructor(canvas)
+    {
+        console.log(canvas);
+        document.addEventListener('keydown', this.OnKeyDown.bind(this));
+        document.addEventListener('keyup', this.OnKeyUp.bind(this));
+
+        this.onDownHandlers = [];
+        this.onUpHandlers = [];
+    }
+
+    AddOnKeyDown(handler)
+    {
+        this.onDownHandlers.push(handler);
+    }
+
+    AddOnKeyUp(handler)
+    {
+        this.onUpHandlers.push(handler);
+    }
+
+    DispatchHandlers(handlers)
+    {
+        for (let i = 0; i < handlers.length; i++)
+        {
+            handlers[i]();
+        }
+    }
+
+    OnKeyDown(event)
+    {
+        this.DispatchHandlers(this.onDownHandlers);
+    }
+
+    OnKeyUp(event)
+    {
+        this.DispatchHandlers(this.onUpHandlers);
+    }
+}
